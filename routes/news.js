97eafbdf51db1479fa8267c278b769f6fc782edb
iis istiqomah <iisistiqomah20@gmail.com>
2023-05-23 08:36:50 +0700

    Get request for NewsAPI data

--- routes/news.js
+++ routes/news.js
@@ -9,7 +9,19 @@ newsRoute.set('views', path.join(__dirname, '..', 'views'))
 
 // Route
 newsRoute.get('/', async(req, res) => {
-    res.status(200).render('news.ejs')
+    // res.status(200).render('news.ejs')
+    try {
+        var url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=1235311a10564850bd45dd4a2400da4c'
+        
+        const getNews = await axios.get(url);
+        // console.log(response);
+        res.render('news.ejs',{ articles: getNews.data.articles})
+
+      } catch (error) {
+        if(error.response) {
+            console.error(error);
+        }
+      }
 })
 
 module.exports = newsRoute;
