11361a451a7e06f9cf24e93dac1b6a9e44ab9f93
iis istiqomah <iisistiqomah20@gmail.com>
2023-05-24 07:01:28 +0700

    Update news.ejs, category.ejs, and navbar.ejs

--- routes/news.js
+++ routes/news.js
@@ -11,7 +11,7 @@ newsRoute.set('views', path.join(__dirname, '..', 'views'))
 newsRoute.get('/', async(req, res) => {
     // res.status(200).render('news.ejs')
     try {
-        var url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=1235311a10564850bd45dd4a2400da4c'
+        var url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=1235311a10564850bd45dd4a2400da4c';
         
         const getNews = await axios.get(url);
         // console.log(getNews);
